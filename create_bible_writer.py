45ed646a0c2ecd3d0546c63999fafe8afbb59b5e
Nander Speerstra <nander.speerstra@infor.com>
2017-02-01 11:02:58 +0100

    Many changes; now able to run both scripts without problems

--- create_bible_writer.py
+++ create_bible_writer.py
@@ -7,18 +7,29 @@ from keras.layers import LSTM
 from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
 
+import tensorflow as tf
+tf.python.control_flow_ops = tf  # some hack to get tf running with Dropout
+
+from keras import backend as K
+K.set_image_dim_ordering('tf') # or 'th' for theano
+
 # load ascii text and covert to lowercase
 filename = "bible_1637.txt"
 raw_text = open(filename).read()
 
 # Create a smaller part to train on (for testing purposes)
-raw_text = raw_text[:2000000]
+raw_text = raw_text[:200]
 
+# All characters in lowercase
 raw_text = raw_text.lower()
 
 # create mapping of unique chars to integers
 chars = sorted(list(set(raw_text)))
 char_to_int = dict((c, i) for i, c in enumerate(chars))
+with open('chars_to_int.txt', 'w') as outChars:
+    for key in char_to_int:
+        outChars.write('{}\t{}\n'.format(key, char_to_int[key]))
+
 
 # summarize the loaded data
 n_chars = len(raw_text)
@@ -27,25 +38,28 @@ print ("Total Characters: ", n_chars)
 print ("Total Vocab: ", n_vocab)
 
 # prepare the dataset of input to output pairs encoded as integers
-seq_length = 50
+seq_length = 30
 dataX = []
 dataY = []
 for i in range(0, n_chars - seq_length, 1):
-	seq_in = raw_text[i:i + seq_length]
-	seq_out = raw_text[i + seq_length]
-	dataX.append([char_to_int[char] for char in seq_in])
-	dataY.append(char_to_int[seq_out])
+    seq_in = raw_text[i:i + seq_length]
+    seq_out = raw_text[i + seq_length]
+        	
+    dataX.append([char_to_int[char] for char in seq_in])
+    dataY.append(char_to_int[seq_out])
 n_patterns = len(dataX)
 print ("Total Patterns: ", n_patterns)
+
 
 # Export dataX and dataY
 with open('dataX.txt', 'w') as outX:
     for thing in dataX:
-        outX.write(thing + '\n')
+        line = ' '.join([str(element) for element in thing])
+        outX.write(line + '\n')
         
 with open('dataY.txt', 'w') as outY:
-    for thing in dataY:
-        outY.write(thing + '\n')  
+	line = ' '.join([str(element) for element in dataY])
+	outY.write(line + '\n')  
 
 # reshape X to be [samples, time steps, features]
 X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
@@ -58,16 +72,28 @@ y = np_utils.to_categorical(dataY)
 
 # define the LSTM model
 model = Sequential()
-model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2])))
+model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
+model.add(LSTM(256))
 model.add(Dropout(0.2))
 model.add(Dense(y.shape[1], activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 
 # define the checkpoint
-filepath="weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
+#filepath="weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
+filepath = "weights-best.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
 callbacks_list = [checkpoint]
 
 # fit the model
 model.summary()
-model.fit(X, y, nb_epoch=5, batch_size=128, callbacks=callbacks_list)
+model.fit(X, y, nb_epoch=1, batch_size=128, callbacks=callbacks_list)
+
+# Write the parameters to a new file
+model_out = open('model_parameters.txt', 'w')
+model_out.write('n_chars\t{}\n'.format(n_chars))
+model_out.write('n_vocab\t{}\n'.format(n_vocab))
+model_out.write('seq_length\t{}\n'.format(seq_length))
+model_out.write('n_patterns\t{}\n'.format(n_patterns))
+model_out.write('X_shape\t{}\t{}\n'.format(X.shape[1], X.shape[2]))
+model_out.write('model_name\t{}\n'.format(filepath))
+model_out.close()
--- create_new_bible_sentence.py
+++ create_new_bible_sentence.py
@@ -1,13 +1,69 @@
+# Small LSTM Network to Generate some Bible
+import sys
+
+import numpy
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import LSTM
+from keras.callbacks import ModelCheckpoint
+from keras.utils import np_utils
+
+import tensorflow as tf
+tf.python.control_flow_ops = tf  # some hack to get tf running with Dropout
+
+from keras import backend as K
+K.set_image_dim_ordering('th') # or 'th' for theano
+
+# Get model parameters
+model_par = open('model_parameters.txt', 'r').readlines()
+parameters = {}
+for line in model_par:
+    line = line.strip()
+    elements = line.split('\t')
+    if len(elements)==3:
+        parameters[elements[0]] = (int(elements[1]),int(elements[2]))
+    elif elements[0] == 'model_name':
+        parameters[elements[0]] = elements[1]
+    else:
+        parameters[elements[0]] = int(elements[1])
+
+# Read dataX
+X = [thing.strip() for thing in open('dataX.txt', 'r').readlines()]
+dataX = []
+for thing in X:
+    dataX.append([int(element.strip()) for element in thing.split(' ')])
+
+# Read dataY
+Y = open('dataY.txt', 'r').readlines()[0]
+dataY = [int(thing) for thing in Y.split(' ')]
+
+# one hot encode the output variable
+y = np_utils.to_categorical(dataY)
+
+# define the LSTM model
+model = Sequential()
+model.add(LSTM(256, input_shape=(parameters['X_shape'][0], parameters['X_shape'][1]), return_sequences=True))
+model.add(LSTM(256))
+model.add(Dropout(0.2))
+model.add(Dense(y.shape[1], activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
 # load the network weights
-filename = "weights-improvement-04-1.6881.hdf5"
+filename = parameters['model_name']
 model.load_weights(filename)
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 
-int_to_char = dict((i, c) for i, c in enumerate(chars))
-
-# Read dataX
-dataX = [thing.strip() for thing in open('dataX.txt', 'r').readlines()]
-#dataY = [thing.strip() for thing in open('dataY.txt', 'r').readlines()]
+# Load chars_to_int
+int_to_char = {}
+with open('chars_to_int.txt', 'r') as get_chars:
+    
+    for line in get_chars:
+        try:
+            line = line.strip('\n').split('\t')
+            int_to_char[int(line[1])] = line[0]
+        except IndexError:
+            print('error')
 
 # pick a random seed
 start = numpy.random.randint(0, len(dataX)-1)
@@ -15,15 +71,21 @@ pattern = dataX[start]
 print ("Seed:\n")
 print (''.join([int_to_char[value] for value in pattern]))
 
+output = ''
 # generate characters
-for i in range(50):
-	x = numpy.reshape(pattern, (1, len(pattern), 1))
-	x = x / float(n_vocab)
-	prediction = model.predict(x, verbose=0)
-	index = numpy.argmax(prediction)
-	result = int_to_char[index]
-	seq_in = [int_to_char[value] for value in pattern]
-	sys.stdout.write(result)
-	pattern.append(index)
-	pattern = pattern[1:len(pattern)]
+for i in range(100):
+    x = numpy.reshape(pattern, (1, len(pattern), 1))
+    x = x / float(parameters['n_vocab'])
+    prediction = model.predict(x, verbose=0)
+    index = numpy.argmax(prediction)
+    result = int_to_char[index]
+    seq_in = [int_to_char[value] for value in pattern]
+    
+    #sys.stdout.write(result)
+    output += result
+    
+    pattern.append(index)
+    pattern = pattern[1:len(pattern)]
+    
+print(output.strip())
 print ("\n\nDone.")
