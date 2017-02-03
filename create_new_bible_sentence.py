ebdb5bc750e4f11fe40ea24751c22119d09ea9d5
Nander Speerstra <nander.speerstra@infor.com>
2017-02-03 07:19:56 +0100

    Added flags to create_new_bible_sentence

--- create_new_bible_sentence.py
+++ create_new_bible_sentence.py
@@ -1,5 +1,6 @@
 # Small LSTM Network to Generate some Bible
 import sys
+import re
 
 import numpy
 from keras.models import Sequential
@@ -15,8 +16,24 @@ tf.python.control_flow_ops = tf  # some hack to get tf running with Dropout
 from keras import backend as K
 K.set_image_dim_ordering('tf') # or 'th' for theano
 
+import argparse
+
+########################################################################
+#
+########################################################################
+# Input parser
+parser = argparse.ArgumentParser(description='Create bible sentence')
+parser.add_argument('--num_sentences', type=int, required=False,
+                                        help='The number of sentences you want to generate.')
+parser.add_argument('--len_sentences', type=int, required=False,
+                                        help='The length of the sentences you want to generate.')
+parser.add_argument('--models_location', type=str, required=True,
+                                        help='The location of the models you created.')
+args = parser.parse_args()
+
+
 # Get model parameters
-model_par = open('model_parameters.txt', 'r').readlines()
+model_par = open(args.models_location+'/model_parameters.txt', 'r').readlines()
 parameters = {}
 for line in model_par:
     line = line.strip()
@@ -29,13 +46,13 @@ for line in model_par:
         parameters[elements[0]] = int(elements[1])
 
 # Read dataX
-X = [thing.strip() for thing in open('dataX.txt', 'r').readlines()]
+X = [thing.strip() for thing in open(args.models_location+'/dataX.txt', 'r').readlines()]
 dataX = []
 for thing in X:
     dataX.append([int(element.strip()) for element in thing.split(' ')])
 
 # Read dataY
-Y = open('dataY.txt', 'r').readlines()[0]
+Y = open(args.models_location+'/dataY.txt', 'r').readlines()[0]
 dataY = [int(thing) for thing in Y.split(' ')]
 
 # one hot encode the output variable
@@ -44,19 +61,18 @@ y = np_utils.to_categorical(dataY)
 # define the LSTM model
 model = Sequential()
 model.add(LSTM(256, input_shape=(parameters['X_shape'][0], parameters['X_shape'][1])))
-#model.add(LSTM(256))
 model.add(Dropout(0.2))
 model.add(Dense(y.shape[1], activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 
 # load the network weights
-filename = parameters['model_name']
+filename = args.models_location +'/' + parameters['model_name']
 model.load_weights(filename)
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 
 # Load chars_to_int
 int_to_char = {}
-with open('chars_to_int.txt', 'r') as get_chars:
+with open(args.models_location+'/chars_to_int.txt', 'r') as get_chars:
     
     for line in get_chars:
         try:
@@ -71,21 +87,30 @@ pattern = dataX[start]
 print ("Seed:\n")
 print (''.join([int_to_char[value] for value in pattern]))
 
-output = ''
-# generate characters
-for i in range(100):
-    x = numpy.reshape(pattern, (1, len(pattern), 1))
-    x = x / float(parameters['n_vocab'])
-    prediction = model.predict(x, verbose=0)
-    index = numpy.argmax(prediction)
-    result = int_to_char[index]
-    seq_in = [int_to_char[value] for value in pattern]
-    
-    #sys.stdout.write(result)
-    output += result
-    
-    pattern.append(index)
-    pattern = pattern[1:len(pattern)]
-    
-print(output.strip())
+
+for sent in range(0,args.num_sentences):
+        # pick a random seed
+        start = numpy.random.randint(0, len(dataX)-1)
+        pattern = dataX[start]
+        print ("Seed:\t{}".format(''.join([int_to_char[value] for value in pattern])))
+        
+        output = ''
+        # generate characters
+        for i in range(args.len_sentences):
+            x = numpy.reshape(pattern, (1, len(pattern), 1))
+            x = x / float(parameters['n_vocab'])
+            prediction = model.predict(x, verbose=0)
+            index = numpy.argmax(prediction)
+            result = int_to_char[index]
+            seq_in = [int_to_char[value] for value in pattern]
+            
+            #sys.stdout.write(result)
+            output += result
+            
+            pattern.append(index)
+            pattern = pattern[1:len(pattern)]
+        
+        output = re.sub('\n', ' ', output)    
+        print(' - {}'.format(output))
+        
 print ("\n\nDone.")
